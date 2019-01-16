35b5d2cc11fa5ff582ceb5d1d6d3f2c9f8b11acf
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-16 12:47:44 +0530

    Handling title only.
    Not handling images.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
+const { readFile } = require('fs');
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  res.statusCode = 200;
+  if (req.url == '/') {
+    readFile('./index.html', 'utf8', (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
+  if (req.url != '/') {
+    res.end();
+  }
 };
 
 // Export a function that can act as a handler
