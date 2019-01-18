ad0e17775066ffd026c633379404795e0ad6837e
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-18 10:49:02 +0530

    Added sheegra.js frameWork and converted app.js to corresponding frameWork.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 const { readFile } = require('fs');
+const Sheegra = require('./sheegra');
+const app = new Sheegra();
 
-const reader = function(path, res, req) {
+const reader = function(req, res) {
+  let path = req.url.slice(1) || 'index.html';
   let root = 'html_page/' + path;
   readFile(root, (err, data) => {
-    if (err) {
-      res.statusCode = 404;
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
+    res.write(data);
+    res.end();
   });
 };
-const app = (req, res) => {
-  reader(req.url.slice(1) || 'index.html', res);
-};
 
+const sendNotFound = function(req, res) {
+  res.statusCode = 404;
+  res.end();
+};
 // Export a function that can act as a handler
 
-module.exports = app;
+app.get('/', reader);
+app.get('/resources/animated-flower-image-0021.gif', reader);
+app.get('/resources/freshorigins.jpg', reader);
+app.get('/style.css', reader);
+app.use(sendNotFound);
+module.exports = app.handleRequest.bind(app);
